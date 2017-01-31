9ae55339d313540598818e7b3ca9c4bda2228b10
Open Panzer <admin@openpanzer.org>
2017-01-30 20:40:27 -0600

    Update default EEPROM values for turret rotation, barrel elevation, and headlight sounds (default to enabled for all three). Default all squeaks to disabled. This makes better sense now we will be supporting more sound cards.

--- libraries/OP_EEPROM/OP_EEPROM.h
+++ libraries/OP_EEPROM/OP_EEPROM.h
@@ -48,7 +48,7 @@
 // and the sketch will exhibit unstable behavior!
 // 
 
-    #define EEPROM_INIT             0xDF03          // Modified with 00.90.22 on 1/11/2017
+    #define EEPROM_INIT             0xAA46          // Modified with 00.90.24 on 1/30/2017
 //
 //
 //=======================================================================================================================================>>
