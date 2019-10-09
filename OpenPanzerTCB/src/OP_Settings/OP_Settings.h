f3b88bd5d64cb8718dd4cf6d92864c391b23c516
Open Panzer <admin@openpanzer.org>
2019-10-09 11:46:43 -0500

    Update to version 0.93.64

--- OpenPanzerTCB/src/OP_Settings/OP_Settings.h
+++ OpenPanzerTCB/src/OP_Settings/OP_Settings.h
@@ -16,9 +16,9 @@
 // OPEN PANZER TANK CONTROL BOARD (TCB) - FIRMWARE VERSION NUMBER
 // ------------------------------------------------------------------------------------------------------------------------------------------------------->>
     // This is the firmware version that will be returned to the desktop application when it checks. It will be split into three, two-digit numbers
-    #define FIRMWARE_VERSION        "0.93.63"      // version. Last update 10/7/2019
+    #define FIRMWARE_VERSION        "0.93.64"      // version. Last update 10/9/2019
     
-    #define MIN_OPCONFIG_VERSION    "0.93.60"      // Minimum version of OP Config this version of firmware requires
+    #define MIN_OPCONFIG_VERSION    "0.93.64"      // Minimum version of OP Config this version of firmware requires
 
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------->>
